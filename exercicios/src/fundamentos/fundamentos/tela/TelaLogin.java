a40ef15905a4524e77c3a1b370b8a5c13e982978
Davi Anderson DaviFeels <davifeels23@gmail.com>
2025-07-06 23:02:56 -0300

    Update TelaLogin.java

--- exercicios/src/fundamentos/fundamentos/tela/TelaLogin.java
+++ exercicios/src/fundamentos/fundamentos/tela/TelaLogin.java
@@ -7,25 +7,25 @@ public class TelaLogin {
 
         Scanner sc = new Scanner(System.in);
 
-        // Usu·rio e senha prÈ-definidos
+        // Usu√°rio e senha pr√©-definidos
         String usuarioCorreto = "admin";
         String senhaCorreta = "1234";
 
         System.out.println("====== TELA DE LOGIN ======\n");
 
-        System.out.print("Usu·rio: ");
+        System.out.print("Usu√°rio: ");
         String usuarioDigitado = sc.nextLine();
 
         System.out.print("Senha: ");
         String senhaDigitada = sc.nextLine();
 
-        // Verifica se usu·rio e senha est„o corretos
+        // Verifica se usu√°rio e senha est√£o corretos
         if (usuarioDigitado.equals(usuarioCorreto) && senhaDigitada.equals(senhaCorreta)) {
             System.out.println("\nLogin realizado com sucesso! ?");
         } else {
-            System.out.println("\nUsu·rio ou senha incorretos. ?");
+            System.out.println("\nUsu√°rio ou senha incorretos. ?");
         }
-
+System.out.println("\nUsu√°rio ou senha incorretos. ?");
         sc.close();
     }
 }
